2855b11e322621dc7d83af5ec667dddc8331b922
Angel <angel.blanco@live.u-tad.com>
2021-07-21 02:27:46 +0200

    first commit

---
+++ js/move.js
@@ -0,0 +1,139 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.navbar', {
+        duration: 500,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.body1h2', {
+        duration: 1550,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.body1h1', {
+        duration: 1550,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.body1h3', {
+        duration: 1550,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.body1p', {
+        duration: 1550,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.movilesStyle1', {
+        duration:1550,
+        origin: 'left',
+        distance: '-100px'
+    });
+
+    sr.reveal('.movilesStyle', {
+        duration:2050,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.divTxtAbout', {
+        duration:2050,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.quotes', {
+        duration: 4050,
+        origin: 'top',
+        distance: '-300px'
+    });
+
+    sr.reveal('.imagenBuilding', {
+        duration:3550,
+        origin: 'top',
+        distance: '-500px'
+    });
+
+    sr.reveal('.movilesStyle', {
+        duration:3550,
+        origin: 'top',
+        distance: '-500px'
+    });
+
+    sr.reveal('.contTextAbout', {
+        duration:3550,
+        origin: 'top',
+        distance: '-500px'
+    });
+
+
+    sr.reveal('.logosEspaciado1', {
+        duration:3550,
+        origin: 'left',
+        distance: '-1000px'
+    });
+
+    sr.reveal('.cajaXP', {
+        duration: 3550,
+        origin: 'rigth',
+        distance: '-1000px'
+    });
+
+    sr.reveal('.ikea', {
+        duration: 3550,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.leroy', {
+        duration: 3550,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.txtAboutp1', {
+        duration: 3550,
+        origin: 'top',
+        distance: '-300px'
+    });
+    sr.reveal('.txtAbout', {
+        duration: 3550,
+        origin: 'top',
+        distance: '-300px'
+    });
+    sr.reveal('.txtAboutp11', {
+        duration: 3550,
+        origin: 'top',
+        distance: '-300px'
+    });
+
+    sr.reveal('.comillas', {
+        duration: 3550,
+        origin: 'right',
+        distance: '-300px'
+    });
+
+    sr.reveal('.pdf', {
+        duration: 2550,
+        origin: 'left',
+        distance: '-300px'
+    });
+
+    sr.reveal('.imgpdf', {
+        duration: 3550,
+        origin: 'left',
+        distance: '-300px'
+    });
+
+    sr.reveal('.divXP', {
+        duration: 3550,
+        origin: 'top',
+        distance: '-300px'
+    });
+
