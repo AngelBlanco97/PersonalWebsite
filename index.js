d2db53c40641ac63b317deefd2aba6ce0697b4af
Ángel Blanco Rodríguez <73697059+AngelBlanco97@users.noreply.github.com>
2021-10-15 13:33:24 +0200

    set time to change theme, visual changes

--- index.js
+++ index.js
@@ -2,11 +2,23 @@
 const btn = document.getElementById("chk");
 
 const theme = document.getElementById("theme-selector");
+theme.style.transition = "all 2 s";
 
 btn.addEventListener("click", function() {
+    tiempo();
+});
+
+
+
+function tiempo(){
+    tiempoEspera = setTimeout(cambioColor, 800);
+}
+
+
+function cambioColor() { 
     if (theme.getAttribute("href") == "light-theme.css") {
         theme.href = "dark-theme.css";
     } else {
         theme.href = "light-theme.css";
     }
-});
+}
